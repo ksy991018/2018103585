165f950b7abd5705088fa48c0b78e36f6487f497
Heo <hjy970507@naver.com>
2021-04-09 16:03:28 +0900

    gugudans

--- test-2018103585.cpp
+++ test-2018103585.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main() {
 	cout << "구구단 시작" << endl<<endl;
 
-	for (int i = 1; i <9; i++){
-		for (int j = 1; j <= 9; j++) {
+	for (int i = 1; i <10; i++){
+		for (int j = 1; j <10; j++) {
 			printf("%d x %d = %d\n", i, j, i * j);
 		}
 		cout << endl;
